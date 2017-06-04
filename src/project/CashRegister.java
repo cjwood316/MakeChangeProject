0854efb2db5ce7c65186ab639d4320d246b99b7f
Christopher Jacob Wood <jacob@Christophers-MacBook-Pro.local>
2017-06-04 15:46:08 -0600

    scanner takes in user input for price and payment recievedw

--- src/project/CashRegister.java
+++ src/project/CashRegister.java
@@ -4,8 +4,22 @@ import java.util.Scanner;
 
 public class CashRegister {
 	Scanner kb = new Scanner(System.in);
-	private int priceOfProduct, moneyProvided, change;
-	private int twenties, tens, fives, ones;
-	private double quarters, dimes, nickels, pennies;
+
+	public static void main(String[] args) {
+		convertChange();
+	}
+
+	public static void convertChange() {
+		int priceOfProduct, moneyProvided, change;
+		int twenties, tens, fives, ones;
+		double quarters, dimes, nickels, pennies;
+
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter purchase price of product: $");
+		priceOfProduct = (int) (kb.nextDouble() * 100);
+		System.out.print("Enter the amount given by the customer: $");
+		moneyProvided = (int) (kb.nextDouble() * 100);
+		kb.close();
+	}
 
 }
