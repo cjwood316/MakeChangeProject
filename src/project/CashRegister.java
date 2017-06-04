fdc10ac5f04ac0e03320fae01ddd9afd4dd1d350
Christopher Jacob Wood <jacob@Christophers-MacBook-Pro.local>
2017-06-04 16:39:12 -0600

    Completed and tested logic for coin denominations

--- src/project/CashRegister.java
+++ src/project/CashRegister.java
@@ -25,25 +25,25 @@ public class CashRegister {
 			System.out.println("The customer should be given the change as follows: ");
 			change = moneyProvided - priceOfProduct;
 			// First test to see if Twenties work. *passed*
-			int twenties = (int)change / 2000;
+			int twenties = (int) change / 2000;
 			if (twenties > 0) {
 				change = change % 2000;
 				System.out.println(twenties + " $20 bill(s)");
 			}
 			// Second test checks for tens. *passed*
-			int tens = (int)change / 1000;
+			int tens = (int) change / 1000;
 			if (tens > 0) {
 				change = change % 1000;
 				System.out.println(tens + " $10 bill(s)");
 			}
 			// Checks for remainder of fives *passed*
-			int fives = (int)change / 500;
+			int fives = (int) change / 500;
 			if (fives > 0) {
 				change = change % 500;
 				System.out.println(fives + " $5 bill(s)");
 			}
 			// Checks for remainder of ones *passed*
-			int ones = (int)change / 100;
+			int ones = (int) change / 100;
 			if (ones > 0) {
 				change = change % 100;
 				System.out.println(ones + " $1 bill(s)");
@@ -51,12 +51,23 @@ public class CashRegister {
 			double quarters = change / 25;
 			if (quarters > 0) {
 				change = change % 25;
-				System.out.println((int)quarters + " quarter(s)");
-
-			} else {
-
+				System.out.println((int) quarters + " quarter(s)");
 			}
+			double dimes = change / 10;
+			if (dimes > 0) {
+				change = change % 10;
+				System.out.println((int)dimes + " dime(s)");
+			}
+			double nickels = change / 5;
+			if (nickels > 0) {
+				change = change % 5;
+				System.out.println((int)nickels + " nickel(s)");
+			}
+			double pennies = change;
+			System.out.println((int)pennies + " pennies");
+
+		} else {
+
 		}
 	}
-
 }
