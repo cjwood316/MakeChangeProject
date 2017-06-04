4d29b098d08562a0842f29ad28187aa4a96c088d
Christopher Jacob Wood <jacob@Christophers-MacBook-Pro.local>
2017-06-04 16:33:03 -0600

    Corrected print out logic for change, and casted change into and int for 1,5,10 and 20 values

--- src/project/CashRegister.java
+++ src/project/CashRegister.java
@@ -10,48 +10,53 @@ public class CashRegister {
 	}
 
 	public static void convertChange() {
-		int priceOfProduct, moneyProvided, change;
+		double priceOfProduct, moneyProvided, change;
 
 		Scanner kb = new Scanner(System.in);
 		System.out.println("Please enter purchase price of product: $");
-		priceOfProduct = (int) (kb.nextDouble() * 100);
+		priceOfProduct = (kb.nextDouble() * 100);
 		System.out.print("Enter the amount given by the customer: $");
-		moneyProvided = (int) (kb.nextDouble() * 100);
+		moneyProvided = (kb.nextDouble() * 100);
 		kb.close();
 
 		// Insert program logic to compare
 		if (moneyProvided > priceOfProduct) {
-			System.out.println("The change is: " + ((moneyProvided - priceOfProduct) / 100));
+			System.out.println("The change is: $" + ((moneyProvided - priceOfProduct) / 100));
 			System.out.println("The customer should be given the change as follows: ");
 			change = moneyProvided - priceOfProduct;
 			// First test to see if Twenties work. *passed*
-			int twenties = change / 2000;
+			int twenties = (int)change / 2000;
 			if (twenties > 0) {
 				change = change % 2000;
 				System.out.println(twenties + " $20 bill(s)");
 			}
 			// Second test checks for tens. *passed*
-			int tens = change / 1000;
+			int tens = (int)change / 1000;
 			if (tens > 0) {
 				change = change % 1000;
 				System.out.println(tens + " $10 bill(s)");
 			}
 			// Checks for remainder of fives *passed*
-			int fives = change / 500;
+			int fives = (int)change / 500;
 			if (fives > 0) {
 				change = change % 500;
 				System.out.println(fives + " $5 bill(s)");
 			}
 			// Checks for remainder of ones *passed*
-			int ones = change / 100;
+			int ones = (int)change / 100;
 			if (ones > 0) {
 				change = change % 100;
 				System.out.println(ones + " $1 bill(s)");
-				// Divided by 100 to counter the multiplication of 100.
+			}
+			double quarters = change / 25;
+			if (quarters > 0) {
+				change = change % 25;
+				System.out.println((int)quarters + " quarter(s)");
 
 			} else {
 
 			}
 		}
 	}
+
 }
